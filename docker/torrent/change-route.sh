b35c307c399e8a3036729d640675cad99552bedd
André Lacasse <alacasse@gmail.com>
2025-11-19 16:05:06 -0500

    fix: set eth0 MTU to 1320 to match WireGuard

--- docker/torrent/change-route.sh
+++ docker/torrent/change-route.sh
@@ -3,3 +3,5 @@ set -euo pipefail
 echo "Changing default route to VPN..."
 ip route del default
 ip route add default via 172.18.0.2
+echo "Setting MTU to 1320 to match WireGuard..."
+ip link set dev eth0 mtu 1320
